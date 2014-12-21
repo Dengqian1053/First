521133efeeefc678130f3245dd9d514f3845d114
Dengqian1053 <dqian1053@gmail.com>
2014-12-21 23:05:08 +0800

    测试一次更新操作，不知道有没有惊喜^_^

--- src/UDPDemoRecevie.java
+++ src/UDPDemoRecevie.java
@@ -1,14 +1,24 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.SocketException;
 
 
 public class UDPDemoRecevie {
-	public static void main(String[] args) throws IOException {
-		DatagramSocket ds = new DatagramSocket(10000);
+	public static void main(String[] args){
+		DatagramSocket ds = null;
+		try {
+			ds = new DatagramSocket(10000);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
 		byte[] buf = new byte[1024];
 		DatagramPacket dp = new DatagramPacket(buf, buf.length);
-		ds.receive(dp);
+		try {
+			ds.receive(dp);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		System.out.println(new String(dp.getData()));
 		ds.close();
 	}
--- src/UDPDemoSend.java
+++ src/UDPDemoSend.java
@@ -2,15 +2,31 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 
 public class UDPDemoSend {
-	public static void main(String[] args) throws IOException {
-		DatagramSocket ds = new DatagramSocket();
+	public static void main(String[] args) {
+		DatagramSocket ds = null;
+		try {
+			ds = new DatagramSocket();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
 		byte[] buf = "hello lin fan! I love you!".getBytes();
-		DatagramPacket dp = new DatagramPacket(buf, buf.length, InetAddress.getByName("127.0.0.1"), 10000);
+		DatagramPacket dp = null;
+		try {
+			dp = new DatagramPacket(buf, buf.length, InetAddress.getByName("127.0.0.1"), 10000);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
 		
-		ds.send(dp);
+		try {
+			ds.send(dp);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		ds.close();
 	}
 }
